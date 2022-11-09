c3d255d6f6d246a65303949e0632a9d4214b973e
NetHunter123 <viktor3@i.ua>
2022-11-09 13:16:06 +0200

    make lr4

--- src/components/ShopCard/index.js
+++ src/components/ShopCard/index.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
 
-const ShopCard = ({title,desc,img}) => {
+// const card = document.getElementsByClassName("shop-card");
+
+const ShopCard = ({ title, desc, img }) => {
   return (
     <Card className={"shop-card"}>
-      <Card.Img variant="top" src={`${img}`} style={{
-          height:"200px"
-      }}/>
-      <Card.Body>
+      <Card.Img
+        variant="top"
+        src={`${img}`}
+        style={{
+          height: "200px",
+        }}
+      />
+      <Card.Body className={"card-body"}>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{desc}</Card.Text>
-        <Button variant="outline-secondary">Go To</Button>
+        <Button
+          variant="outline-secondary"
+          className={"card-btn"}
+          onClick={(e) => {
+            // console.log("event: ", e.target.parentNode.parentNode.classes);
+            e.target.parentNode.parentNode.classList.toggle("card-active");
+          }}
+        >
+          Add
+        </Button>
       </Card.Body>
     </Card>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,67 +2,67 @@ import React from "react";
 import ShopCard from "../components/ShopCard";
 
 const Home = () => {
-    const goods = [
-        {
-            id: 1,
-            title:
-                "Lorem ipsum ",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, quibusdam.",
-            img: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-        },
-        {
-            id: 2,
-            title:
-                "Lorem ipsum dolor sit ",
-            desc: "Lorem ipsum dolor sit amet, consectetur. Illum, quibusdam.",
-            img: "https://images.unsplash.com/photo-1454817481404-7e84c1b73b4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        },
-        {
-            id: 3,
-            title:
-                "Lorem ipsum dolor ",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum.",
-            img: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-        },
-        {
-            id: 4,
-            title:
-                "Lorem dolor amet, adipisicing elit. Illum, quibusdam.",
-            desc: "Lorem sit amet, consectetur adipisicing elit. Illum, quibusdam.",
-            img: "https://images.unsplash.com/photo-1454817481404-7e84c1b73b4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        },
-        {
-            id: 5,
-            title:
-                "Lorem ipsum dolor.",
-            desc: "Lorem ipsum dolor sit amet, Illum, quibusdam.",
-            img: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-        },
-        {
-            id: 6,
-            title:
-                "consectetur adipisicing elit. Illum, quibusdam.",
-            desc: "Lorem ipsum dolor, quibusdam.",
-            img: "https://images.unsplash.com/photo-1454817481404-7e84c1b73b4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        },
-    ];
+  const goods = [
+    {
+      id: 1,
+      title: "Lorem ipsum ",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, quibusdam.",
+      // img: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      img: "/img/foto1.jpg",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit ",
+      desc: "Lorem ipsum dolor sit amet, consectetur. Illum, quibusdam.",
+      // img: "https://images.unsplash.com/photo-1454817481404-7e84c1b73b4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      img: "/img/foto2.jpg",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor ",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum.",
+      // img: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      img: "/img/foto3.jpg",
+    },
+    {
+      id: 4,
+      title: "Lorem dolor amet, adipisicing elit. Illum, quibusdam.",
+      desc: "Lorem sit amet, consectetur adipisicing elit. Illum, quibusdam.",
+      img: "/img/foto4.jpg",
+    },
+    {
+      id: 5,
+      title: "Lorem ipsum dolor.",
+      desc: "Lorem ipsum dolor sit amet, Illum, quibusdam.",
+      img: "/img/foto5.jpg",
+    },
+    {
+      id: 6,
+      title: "consectetur adipisicing elit. Illum, quibusdam.",
+      desc: "Lorem ipsum dolor, quibusdam.",
+      img: "/img/foto6.jpg",
+    },
+  ];
 
-    return (
-        <>
-            <div className={"home"}>
-                <div className="goods">
-                    {goods.map((item) => {
-                        return (
-                            <>
-                               <ShopCard img={item.img} title={item.title} desc={item.desc}/>
-                            </>
-                        );
-                    })}
-                </div>
-            </div>
-            ;
-        </>
-    );
+  return (
+    <>
+      <div className={"home"}>
+        <div className="goods">
+          {goods.map((item, i = 1) => {
+            return (
+              <ShopCard
+                key={i++}
+                img={item.img}
+                title={item.title}
+                desc={item.desc}
+              />
+            );
+          })}
+        </div>
+      </div>
+      ;
+    </>
+  );
 };
 
 export default Home;
